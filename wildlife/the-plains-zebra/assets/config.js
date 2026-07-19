f6765952d0f41b6b32e507b0ed770c81ef0215f5
grvolv <188262626+grvolv@users.noreply.github.com>
2026-07-19 18:07:06 -0500

    Add Plains Zebra wildlife demo

---
+++ wildlife/the-plains-zebra/assets/config.js
@@ -0,0 +1,5 @@
+window.PLAINS_ZEBRA_MEDIA = {
+  bilingualVideo: "https://drive.google.com/file/d/1DXAd1sPeDmJISvFW9SpgJB-RsUPj1e6X/view?usp=drive_open",
+  englishVideo: "https://drive.google.com/file/d/1je65TZJoHewS352L75XblracnKzWB_IQ/view?usp=drive_open",
+  ukrainianVideo: "https://drive.google.com/file/d/1G4alCvNSroDGtBmFHjuq3-TrsiRPuqMa/view?usp=drive_open"
+};
