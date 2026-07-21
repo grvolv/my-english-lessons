c2ce3091d09997c11f0cc35890ccafac6be7dc12
grvolv <188262626+grvolv@users.noreply.github.com>
2026-07-21 12:29:46 -0500

    Publish The Plains Zebra es-MX

---
+++ wildlife/the-plains-zebra/es-MX/assets/config.js
@@ -0,0 +1,5 @@
+window.LANGUAGE_MEDIA = {
+  "english": "https://drive.google.com/open?id=1rBTPTjrWeVwsPAp8wHN3vidLcrpbnv3g",
+  "target": "https://drive.google.com/open?id=1s_LR79vn_aVTuM1QdmfzDQrRWBqNIraQ",
+  "bilingual": "https://drive.google.com/open?id=1T-aUL40PBCrZgFMei-LHIdw8nDjJydI9"
+};
